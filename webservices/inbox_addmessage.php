529ee84123992f3f50d97ba61be37076daad7f2f
Thomas ochman <thomas@nocebo.se>
2013-10-04 09:16:34 +0200

    Added Webservices 0.1

---
+++ webservices/inbox_addmessage.php
@@ -0,0 +1,106 @@
+<?php
+
+//load and connect to MySQL database stuff
+require("config.inc.php");
+
+if (!empty($_POST)) {
+	//initial query
+	// This one is not that straight forward as adding a conversation. Here we must check who is the sender of the message to set the rigt flag in `read`, `viewed` & `deleted`.
+	// `notificationSent` is just used to notuify the user in the webintercase so it has to be set to 0 every time there is a new message.
+
+	$query = "START TRANSACTION;
+	INSERT INTO ow_mailbox_message ( conversationId, timeStamp, senderId, recipientId, text ) VALUES ( :conversationId, :timeStamp, :senderId, :recipientId, :text );
+
+	UPDATE ow_mailbox_conversation
+	SET
+
+	 `read`=
+(CASE
+ WHEN
+ `interlocutorId` = :senderId
+ THEN
+ 2
+ WHEN
+ `initiatorId` = :senderId
+ THEN
+ 1
+ END),
+
+
+     `viewed` =
+     (CASE
+ WHEN
+ `interlocutorId` = :senderId
+ THEN
+ 2
+ WHEN
+ `initiatorId` = :senderId
+ THEN
+ 1
+ END),
+ 
+     `deleted` = IF(`deleted` = '1', '0', '0') ,
+     `deleted` = IF(`deleted` = '2', '0', '0') ,
+     `notificationSent` = '0' ,
+     `subject`= 'for testing...'
+	 WHERE ow_mailbox_conversation.id = :conversationId;
+
+
+	COMMIT;";
+
+	//Update query
+	$query_params = array(
+
+		':conversationId' => $_POST['conversationId'],
+		':timeStamp' => $_POST['timeStamp'],
+		':senderId' => $_POST['senderId'],
+		':recipientId' => $_POST['recipientId'],
+		':text' => $_POST['text'] . "<br><span class='ow_tiny'> - sent with <a href='http://tochman.github.io/OxwallMessenger'>OxwallMessenger</a></span>"
+
+	);
+
+	//execute query
+	try {
+		$stmt   = $db->prepare($query);
+		$result = $stmt->execute($query_params);
+	}
+	catch (PDOException $ex) {
+		// For testing, you could use a die and message.
+		//die("Failed to run query: " . $ex->getMessage());
+
+		//or just use this use this one:
+		$response["success"] = 0;
+		$response["message"] = "Database Error. Couldn't send message!";
+		die(json_encode($response));
+	}
+
+	$response["success"] = 1;
+	$response["message"] = "Message Successfully Added!";
+	echo json_encode($response);
+
+} else {
+
+	$date = date_create();
+?>
+		<h1>Add Comment</h1>
+		<form action="inbox_addmessage.php" method="post">
+			conversationId:<br />
+		    <input type="text" name="conversationId" placeholder="conversationId" />
+		    <br /><br />
+		    timeStamp:<br />
+		    <input type="text" name="timeStamp" placeholder="timeStamp" value="<?php echo $date->format('U'); ?>" />
+		    senderId:<br />
+		    <input type="text" name="senderId" placeholder="senderId" />
+		    <br /><br />
+		    recipientId:<br />
+		    <input type="text" name="recipientId" placeholder="recipientId" />
+		    <br /><br />
+			Message:<br />
+		    <input type="text" name="text" placeholder="text" />
+		    <br /><br />
+		    <input type="submit" value="Add Message" />
+		</form>
+	<?php
+}
+
+?>
---
+++ webservices/members.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+Our "config.inc.php" file connects to database every time we include or require
+it within a php script.  Since we want this script to add a new user to our db,
+we will be talking with our database, and therefore,
+let's require the connection to happen:
+
+The query structure is complicated. Make documentation in wili
+
+LIMIT 0, 100
+
+
+
+
+*/
+require("config.inc.php");
+
+
+if($_GET) {
+//initial query
+$query = "
+
+SELECT 
+		ow_base_user.id, 
+		ow_base_user.username,
+		
+		ow_base_user.password, 
+		ow_base_user.email,
+		ow_base_user.joinStamp, 
+		ow_base_avatar.hash,
+		ow_base_component_entity_setting.value,
+			max(case when questionName = 'sex' then ow_base_question_data.intValue end) as sex,
+			max(case when questionName = 'realname' then ow_base_question_data.textValue end) as realname,
+			max(case when questionName = 'birthdate' then ow_base_question_data.dateValue end) as birthdate
+		
+FROM 
+	ow_base_user  
+LEFT JOIN
+     ow_base_avatar 
+ON 
+	ow_base_user.id = ow_base_avatar.userId
+
+LEFT JOIN
+	ow_base_component_entity_setting
+ON
+	ow_base_user.id = ow_base_component_entity_setting.entityId
+	
+LEFT JOIN
+     ow_base_question_data 
+ON 
+	ow_base_user.id = ow_base_question_data.userId 
+AND
+     questionName in ('sex', 'birthdate', 'realname')
+
+WHERE ow_base_user.username LIKE :search
+    	
+GROUP BY ow_base_user.username
+
+
+
+";
+
+$query_params = array(
+    	':search' => $_GET['search'].'%'
+        //':username' => $_POST['username']
+        
+    );
+
+//execute query
+try {
+    $stmt   = $db->prepare($query);
+    $result = $stmt->execute($query_params);
+}
+catch (PDOException $ex) {
+    $response["success"] = 0;
+    $response["message"] = "Database Error!";
+    die(json_encode($response));
+}
+
+
+
+$member_count = 0;
+while ($row = $stmt->fetch()) {
+	
+  $member_count = $member_count +1;
+  $rows[] = $row;
+};
+
+if ($rows) {
+    $response["success"] = 1;
+    $response["message"] = "Post Available!";
+    $response["count"] = $member_count;
+    $response["posts"]   = array();
+    
+    foreach ($rows as $row) {
+        $post             = array();
+		$post["id"]  = $row["id"];
+        $post["username"] = $row["username"];
+        $post["realname"]    = $row["realname"];
+        $post["email"]    = $row["email"];
+        $post["avatar"] = OW_URL_IMAGE . 'ow_userfiles/plugins/base/avatars/avatar_'.$row['id'].'_'.$row['hash'].'.jpg';
+        
+        
+        
+        //update our repsonse JSON data
+        array_push($response["posts"], $post);
+    }
+    
+    // echoing JSON response
+    echo json_encode($response);
+    
+    
+} else {
+    $response["success"] = 0;
+    $response["message"] = "No Post Available!";
+    die(json_encode($response));
+}
+
+} else {
+	
+echo "Nothing to see here";	
+}
+
+?>
